d18b67101be97de7b044f62e5d8a042dcb87dfa6
kaushikjagadeshan <120910776+kaushikjagadeshan@users.noreply.github.com>
2025-03-20 14:49:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import OCRQRScanner from "./OCRQRScanner";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>OCR & QR Code Scanner</h1>
+      <OCRQRScanner />
+    </div>
+  );
+}
+
+export default App;
